4322f011b61f4c47f5cc81fa30060bf781a26cda
emilianojtoscan <emiliano.j.toscan@gmail.com>
2026-05-07 19:28:09 -0300

    actualizacion

--- php/main_index.php
+++ php/main_index.php
@@ -1,14 +1,17 @@
 <main>
     <div class="flex-column img_fnd">
         <h1 class="text-center mt-5 titulo">Bienvenido a la Grieta del Invocador</h1>
-        <a href="" class="text-center">
-            <img src="../imagenes/cofre (1).png" alt="" height="100">
+        <a href="#" class="text-center text-decoration-none" id="cof">
+            <img src="../imagenes/cofre (1).png" alt="" height="100"><br>
+            16hs Restantes <br>Adelanta por <img src="../imagenes/coin.png" alt="" height="30"> x 65
         </a>
         <div class="container my-5">
             <div class="row justify-content-center">
                 <div class="col-md-3 mb-3">
                     <button type="button" class="btn btn-dark w-100 btn-lg" >Buscar partida</button>
                 </div>
+            </div>
+        </div>
     </div>
     
 </main>
--- php/tienda.php
+++ php/tienda.php
@@ -10,19 +10,29 @@
             <div class="col-md-3 mb-3">
                 <div class="card card_css d-flex flex-row aling-items-center">
                     <img src="../imagenes/Captura de pantalla 2026-05-07 003006.png" alt="" width="100" >
-                    <h5>Skin</h5>
+                    <div class="card-header">
+                        <h5 class="card-title">Skin <br>Summitseeker Evie</h5>
+                        <p></p>
+                        <p id="p_card"><img src="../imagenes/coin.png" alt="" width="40">x 1.200</p>
+                    </div>
                 </div>
             </div>
             <div class="col-md-3 mb-3">
                 <div class="card card_css d-flex flex-row aling-items-center">
                     <img src="../imagenes/Captura de pantalla 2026-05-07 002924.png" alt="" width="100"  >
-                    <h5>Skin</h5>
+                    <div class="card-header">
+                        <h5 class="card-title">Skin <br>Star Oakley</h5>
+                        <p id="p_card"><img src="../imagenes/coin.png" alt="" width="40">x 1.200</p>
+                    </div>
                 </div>
             </div>
             <div class="col-md-3 mb-3">
                 <div class="card card_css d-flex flex-row aling-items-center">
                     <img src="../imagenes/Captura de pantalla 2026-05-07 003016.png" alt="" width="100" >
-                    <h5>Skin</h5>
+                    <div class="card-header">
+                        <h5 class="card-title">Skin <br> The Ageless Explorer</h5>
+                        <p id="p_card"><img src="../imagenes/coin.png" alt="" width="40">x 1.200</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -31,19 +41,28 @@
             <div class="col-md-3 mb-3">
                 <div class="card card_css d-flex flex-row aling-items-center">
                     <img src="../imagenes/Captura de pantalla 2026-05-07 003129.png" alt="" width="100" >
-                    <h5>Skin</h5>
+                    <div class="card-header">
+                        <h5 class="card-title">Skin <br>Major Lazer</h5>
+                        <p id="p_card"><img src="../imagenes/coin.png" alt="" width="40">x 1.200</p>
+                    </div>
                 </div>
             </div>
             <div class="col-md-3 mb-3">
                 <div class="card card_css d-flex flex-row aling-items-center">
                     <img src="../imagenes/Captura de pantalla 2026-05-07 003106.png" alt="" width="100"  >
-                    <h5>Skin</h5>
+                    <div class="card-header">
+                        <h5 class="card-title">Skin <br> Travis Scoot</h5>
+                        <p id="p_card"><img src="../imagenes/coin.png" alt="" width="40">x 1.200</p>
+                    </div>
                 </div>
             </div>
             <div class="col-md-3 mb-3">
                 <div class="card card_css d-flex flex-row aling-items-center">
                     <img src="../imagenes/Captura de pantalla 2026-05-07 003117.png" alt="" width="100" >
-                    <h5>Skin</h5>
+                    <div class="card-header">
+                        <h5 class="card-title">Skin <br>Marshmello</h5>
+                        <p id="p_card"><img src="../imagenes/coin.png" alt="" width="40">x 1.200</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -52,19 +71,28 @@
             <div class="col-md-3 mb-3">
                 <div class="card card_css d-flex flex-row aling-items-center">
                     <img src="../imagenes/Captura de pantalla 2026-05-07 003023.png" alt="" width="100" >
-                    <h5>Skin</h5>
+                    <div class="card-header">
+                        <h5 class="card-title">Skin<br>Sunspot</h5>
+                        <p id="p_card"><img src="../imagenes/coin.png" alt="" width="40">x 1.200</p>
+                    </div>
                 </div>
             </div>
             <div class="col-md-3 mb-3">
                 <div class="card card_css d-flex flex-row aling-items-center">
                     <img src="../imagenes/Captura de pantalla 2026-05-07 004251.png" alt="" width="100"  >
-                    <h5>Skin</h5>
+                    <div class="card-header">
+                        <h5 class="card-title">Skin <br>Shady Doggo</h5>
+                        <p id="p_card"><img src="../imagenes/coin.png" alt="" width="40">x 1.200</p>
+                    </div>
                 </div>
             </div>
             <div class="col-md-3 mb-3">
                 <div class="card card_css d-flex flex-row aling-items-center">
                     <img src="../imagenes/Captura de pantalla 2026-05-07 004258.png" alt="" width="100">
-                    <h5>Skin</h5>
+                    <div class="card-header">
+                        <h5 class="card-title">Skin<br>Unchained Ramirez</h5>
+                        <p id="p_card"><img src="../imagenes/coin.png" alt="" width="40">x 1.200</p>
+                    </div>
                 </div>
             </div>
         </div>
